3f390aa4a70284080e84657893ad10486699f0f7
andhm <andhm@126.com>
2017-06-01 13:34:06 +0800

    add LT_TIME_TYPE...

--- qlimiter.h
+++ qlimiter.h
@@ -10,12 +10,15 @@
 #define DEFAULT_MAX_VAL	(1<<(8*sizeof(long)))
 #define DEFAULT_MIN_VAL	(1>>(8*sizeof(long)))
 
-#define LT_TIME_TYPE_NONE	0
-#define LT_TIME_TYPE_SEC	1
-#define LT_TIME_TYPE_MIN 	2
-#define LT_TIME_TYPE_HOUR	3
-#define LT_TIME_TYPE_DAY	4
+#define LT_TIME_TYPE_NONE	(1<<0)
+#define LT_TIME_TYPE_SEC	(1<<1)
+#define LT_TIME_TYPE_MIN 	(1<<2)
+#define LT_TIME_TYPE_HOUR	(1<<3)
+#define LT_TIME_TYPE_DAY	(1<<4)
+#define LT_TIME_TYPE_5SEC	(1<<5)
+#define LT_TIME_TYPE_10SEC	(1<<6)
 
+//#define DEBUG
 #ifdef DEBUG
 #define LT_DEBUG(fmt, arg...) php_printf("[DEBUG][%s:%d]" fmt "\n", __FILE__, __LINE__, ##arg)
 #else
@@ -23,15 +26,15 @@
 #endif
 
 typedef struct limiter_s {
+	pthread_mutex_t mutex;
 	long curr_val;
 	long init_val;
-	long time;
+	unsigned long time;
 	int time_type;
-	int in_use;
-	pthread_mutex_t mutex;
+	short in_use;
 } limiter_t;
 
-int limiter_incr(char *key, int step, long initval, long maxval, long *retval, long time, int time_type, pthread_mutex_t *smutex);
+int limiter_incr(char *key, int step, long initval, long maxval, long *retval, int time_type, pthread_mutex_t *smutex);
 int limiter_decr(char *key, int step, long initval, long minval);
 long limiter_get(char *key);
 void limiter_delete(char *key);
